d338620a7e698b8c91cd3c75ae1cfca4b0e1eef1
BrandoliOrname <brandonalexisfrancof@gmail.com>
2025-12-14 18:12:06 -0500

    Final

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"com.example.myfirstnavalbattle","l":"com.example.myfirstnavalbattle"}];updateSearchResults();
---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.example.myfirstnavalbattle","l":"Main"}];updateSearchResults();
